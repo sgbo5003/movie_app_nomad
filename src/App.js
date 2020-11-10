c9774a90bdaa6acc40bb0e70de2cb9957b62def0
sgbo5003 <sgbo5003@naver.com>
2020-11-10 20:07:03 +0900

    11/10 공부내용(~ #6 ROUTING BONUS, 마무리

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
     //exact={ture} 를 해야 path의 경로와 정확히 일치하는 component만 보여준다. 하지않으면 전부 보여준다.
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
